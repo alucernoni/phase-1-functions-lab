8f0d2da18fd548e5b57c1bef64b2a0a435a52f0a
Annemarie <a.lucernoni0@gmail.com>
2022-04-25 19:25:35 -0700

    commit tree fa204e459c690fdf83edf9348932d53d04d9fd1b

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+
+const HQ = 42
+function distanceFromHqInBlocks(destination) {
+    if (HQ >= destination) {
+        return HQ - destination;
+    }
+    if (HQ < destination) {
+        return destination - HQ;
+    }
+}
+
+
+function distanceFromHqInFeet(destination) {
+    return distanceFromHqInBlocks(destination) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = Math.abs(start - destination)
+    let feetTravelled = blocksTravelled * 264
+
+    return feetTravelled
+}
+
+
+function calculatesFarePrice(start, destination) {
+    let blocksTravelled = Math.abs(start - destination)
+    let feetTravelled = blocksTravelled * 264
+    let cost ;
+
+    switch(true) {
+        case feetTravelled <= 400:
+            cost = 0;
+            break;
+        case feetTravelled > 400 && feetTravelled <= 2000:
+            cost = (feetTravelled - 400) * .02;
+            break;
+        case feetTravelled > 2000 && feetTravelled <= 2500:
+            cost = 25;
+            break;
+        case feetTravelled > 2500:
+            cost = 'cannot travel that far';
+            break;
+    }
+    return cost
+}
+
+
